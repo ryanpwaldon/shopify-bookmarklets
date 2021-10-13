13ff02f14838637d88d45ee6b050167502620a02
Ryan Waldon <ryanpwaldon@gmail.com>
2021-10-13 20:49:28 +1100

    feat: updates

--- src/bookmarklets/bag-installer/index.ts
+++ src/bookmarklets/bag-installer/index.ts
@@ -4,8 +4,17 @@ class Bookmarklet {
   }
 
   async start() {
+    // const cartButtons = Array.from(document.querySelectorAll('[href$="/cart"]'))
+    // for (const cartButton of cartButtons) {
+    //   const cartButtonClone = cartButton.cloneNode(true)
+    //   cartButton.replaceWith(cartButtonClone)
+    // }
+    const cartButton = document.querySelector('href$="#cart-dropdown"') as Element
+    cartButton.setAttribute('href', '/cart')
+    const cartButtonClone = cartButton.cloneNode(true)
+    cartButton.replaceWith(cartButtonClone)
     const script = document.createElement('script')
-    script.src = 'https://bag-plugin-production.netlify.app/start.js'
+    script.src = 'https://localhost:8080/start.js'
     document.head.appendChild(script)
   }
 }
